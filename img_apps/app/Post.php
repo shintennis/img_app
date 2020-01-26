3d249812e21a74c47d405b7ca3ce4b44b98e7906
shinsaku <shin.tennis.1126@gmail.com>
2020-01-27 03:27:22 +0900

    画像投稿アプリ

---
+++ img_apps/app/Post.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    //
+    Public function user()
+  {
+    return $this->belongsTo('App\User');
+  }
+
+   //hasMany設定
+   public function likes()
+   {
+     return $this->hasMany('App\Like');
+   }
+
+   Public function likedBy($user)
+  {
+    return Like::where('user_id', $user->id)->where('post_id', $this->id);
+  }
+}
---
+++ img_apps/resources/views/layouts/app.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+  <head>
+    <title>{{ config('app.name', 'Laravelgram') }}</title>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    
+    <!--bootstrap-->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <!-- Styles -->
+    <link href="{{ asset('css/application.css') }}" rel="stylesheet">
+
+  </head>
+
+  <body>
+    @yield('navbar')
+       
+       <div class="container">
+         @yield('content')
+       </div>
+       @yield('footer')
+
+  </body>
+</html>
